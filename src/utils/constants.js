55ea75a4eadcee34fe9d13cb3f36f12dd470603f
Mahesh Pulivarthi <pulivarthimahesh1@gmail.com>
2025-07-25 18:39:45 +0530

    Commented TMDB KEY

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,9 +8,7 @@ export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YzEwNGQ1ZWNmOGVlNDdiODAwMDRjNzM3OTA1ZDI0ZSIsIm5iZiI6MTc1MzI0ODY5OS41MDMsInN1YiI6IjY4ODA3M2JiZGFhODY5ZWQ2NTE2YWVkMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.24KW7JwNIjOZnDkfOGuNCwjkCGiLVfOvVFbLFpjjiYo",
-    // + import.meta.env.REACT_APP_TMDB_KEY,
+    Authorization: "Bearer " + import.meta.env.REACT_APP_TMDB_KEY,
   },
 };
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
